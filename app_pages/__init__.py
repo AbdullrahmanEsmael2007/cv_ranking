86dbbfb22efc5b665c7bdb2a38a94541daba027f
abdullrahmanesmael07 <a.esmael@students.htl-leonding.ac.at>
2025-02-12 05:39:42 +0300

    fixed init.py

--- app_pages/__init__.py
+++ app_pages/__init__.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from streamlit_extras.bottom_container import bottom
-from chatgpt_request import request
-from get_job_description import get_job_description
+from app_pages.chatgpt_request import request
+from app_pages.get_job_description import get_job_description
 import PyPDF2
 from openai import OpenAI
 import pandas as pd
 import numpy as np
-from extract_docx_pdf_txt import extract_text_from_pdf, extract_text_from_docx, extract_text_from_txt
+from app_pages.extract_docx_pdf_txt import extract_text_from_pdf, extract_text_from_docx, extract_text_from_txt
 import base64
 import re
 import docx
