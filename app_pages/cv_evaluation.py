3c9a97cad5278b0ea255f71d39c270d424a31383
abdullrahmanesmael07 <a.esmael@students.htl-leonding.ac.at>
2025-02-12 05:13:41 +0300

    fixing error in import

--- app_pages/cv_evaluation.py
+++ app_pages/cv_evaluation.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-from chatgpt_request import request
+
+
+from app_pages import chatgpt_request
+from app_pages.chatgpt_request import request
 from extract_docx_pdf_txt import extract_text_from_pdf, extract_text_from_docx, extract_text_from_txt
 
 def cv_evaluation():
